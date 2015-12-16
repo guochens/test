98a32b299ee60c1f44190f5c2f9a5453ad936a39
Guochen Sun <guochen_s@hotmail.com>
2015-12-16 21:49:04 +0800

    test

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello world\n");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
